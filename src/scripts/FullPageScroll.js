96f666628a0eec7ce180b4623a80ca5712fa42ea
YanG-6668 <goch.yannn@gmail.com>
2020-12-26 14:03:27 +0200

    upgrade

--- src/scripts/FullPageScroll.js
+++ src/scripts/FullPageScroll.js
@@ -2,129 +2,132 @@ export default class FullPageScroll {
   constructor(selector, options) {
     this.root = document.querySelector(selector);
     this.sections = this.root.querySelectorAll('.section');
-    this.options = options;
+    this.navigation;
+    //
+    this.dots = options.dots;
+    this.duration = options.duration || 500;
+    this.timingFunction = options.timingFunction || 'linear';
+    this.onLeave = options.onLeave;
+    this.afterLoad = options.afterLoad;
+    //
     this.activeSlide = 0;
-    this.clickTarget;
-    this.setup();
     this.animated = false;
-    this.navigationLinks = document.querySelectorAll('.navigation__link');
     this.maxSlide = this.sections.length;
+    this.startTouchEvent;
+    this.endTouchEvent;
+
+    this.setup();
   }
 
   createNavigation() {
-    const navigation = document.createElement('nav');
-    navigation.id = navigation.classList = 'navigation';
+    this.navigation = document.createElement('nav');
+    this.navigation.id = this.navigation.classList = 'navigation';
     const navigationList = document.createElement('ul');
     navigationList.classList = 'navigation__list';
-    navigation.appendChild(navigationList);
+    this.navigation.appendChild(navigationList);
     let navigationItem = '';
     this.sections.forEach((item, i) => {
-      navigationItem += `<li class="navigation__item"><a data-navigation=${i + 1} href="#" class="navigation__link"></a></li>`
+      navigationItem +=
+        `<li class="navigation__item">
+        <a data-navigation=${i} href="#" class="navigation__link"></a>
+      </li>`
     });
     navigationList.innerHTML = navigationItem;
     navigationList.querySelectorAll('.navigation__link')[0].classList.add('is-active');
-
-    if (this.options.dots && true) {
-      this.root.appendChild(navigation);
-    } else {
-      return;
-    }
   }
 
   activeNavigationItem(index) {
-    if(this.options.dots && true) {
-      this.navigationLinks[index].classList.add('is-active');
-      this.navigationLinks.forEach((item, i) => {
-        if (index !== i) {
-          this.navigationLinks[i].classList.remove('is-active');
-        }
-      });
-    } else {
-      return;
-    }
+    const navigationLinks = document.querySelectorAll('.navigation__link');
+
+    navigationLinks[index].classList.add('is-active');
+    navigationLinks.forEach((item, i) => {
+      if (index !== i) {
+        navigationLinks[i].classList.remove('is-active');
+      }
+    });
   }
 
-  clickScroll() {
-    document.querySelectorAll('.navigation__link').forEach(link => {
-      link.addEventListener('click', e => {
-        const el = e.target.dataset.navigation;
-        if ((this.activeSlide + 1) !== el) {
-          this.animated = true;
-          this.activeSlide = el - 1;
-          this.sections.forEach(section => {
-            section.style.transform = `translateY(${-(el - 1) * 100}%)`;
-            section.style.transition = `transform ${(this.options.duration / 1000) || 0.5}s ${this.options.timingFunction ? this.options.timingFunction : 'linear'}`;
-          });
-          setTimeout(() => {
-            this.animated = false;
-          }, this.options.duration || '0.5s');
-          this.activeNavigationItem(this.activeSlide);
-        }
-      });
+  actionToSlide() {
+    this.animated = true;
+    this.sections.forEach(section => {
+      section.style.transform = `translateY(${-this.activeSlide * 100}%)`;
+      section.style.transition = `transform ${this.duration}ms ${this.timingFunction}`;
     });
   }
 
   slideUp() {
-    if (this.activeSlide !== 0 && this.animated === false) {
-      this.animated = true;
+    if (this.activeSlide !== 0 && !this.animated) {
       this.activeSlide--;
-      this.sections.forEach(section => {
-        section.style.transform = `translateY(${-this.activeSlide * 100}%)`;
-        section.style.transition = `transform ${(this.options.duration / 1000) || 0.5}s ${this.options.timingFunction ? this.options.timingFunction : 'linear'}`;
-      });
-      setTimeout(() => {
-        this.animated = false;
-      }, this.options.duration || '0.5s');
+      this.actionToSlide();
+      this.timeoutBetweenSlides();
       this.activeNavigationItem(this.activeSlide);
+      this.onLeave()
     }
   }
 
   slideDown() {
-    if ((this.activeSlide + 1) < this.maxSlide && this.animated === false) {
-      this.animated = true;
+    if ((this.activeSlide + 1) < this.maxSlide && !this.animated) {
       this.activeSlide++;
-      this.sections.forEach(section => {
-        section.style.transform = `translateY(${-this.activeSlide * 100}%)`;
-        section.style.transition = `transform ${(this.options.duration / 1000) || 0.5}s ${this.options.timingFunction ? this.options.timingFunction : 'linear'}`;
-      });
-      setTimeout(() => {
-        this.animated = false;
-      }, this.options.duration || '0.5s');
+      this.actionToSlide();
+      this.timeoutBetweenSlides();
       this.activeNavigationItem(this.activeSlide);
+      this.onLeave()
     }
   }
 
-  touchMove() {
-    this.sections.forEach( section => {
-      const touchArr = [];
-      section.addEventListener('touchmove', e => {
-        touchArr.push(e.changedTouches[0].screenY);
-        console.log(e.changedTouches[0].screenY);
+  toSlide(e) {
+    if (this.activeSlide !== e && !this.animated) {
+      this.activeSlide = e;
+      this.actionToSlide();
+      this.timeoutBetweenSlides();
+      this.activeNavigationItem(this.activeSlide);
+      this.onLeave()
+    }
+  }
 
-        if(touchArr[0] > touchArr[touchArr.length - 1]) {
-          console.log('down');
-          this.slideDown();
-          } else {
-          this.slideUp()
-        }
-      });
-    });
+  timeoutBetweenSlides() {
+    setTimeout(() => {
+      this.animated = false;
+    }, this.duration);
   }
 
   setup() {
+    this.afterLoad();
     this.createNavigation();
-    this.clickTarget = this.clickScroll();
 
-    this.touchMove();
+
+    if (this.dots) {
+      this.root.appendChild(this.navigation);
+    }
+
+    //events---------------------->>>
+    document.querySelectorAll('.section__anchors').forEach(anchor => {
+      anchor.addEventListener('click', e => {
+        const target = Number(e.target.dataset.toslide);
+        this.toSlide(target);
+      });
+    });
+
+    document.querySelectorAll('.navigation__link').forEach(link => {
+      link.addEventListener('click', e => {
+        const target = Number(e.target.dataset.navigation);
+        this.toSlide(target);
+      });
+    });
+
+    this.root.addEventListener('touchstart', e => {
+      this.startTouchEvent = e.changedTouches[0].pageY;
+    });
+
+    this.root.addEventListener('touchend', e => {
+      this.endTouchEvent = e.changedTouches[0].pageY;
+      this.startTouchEvent > this.endTouchEvent ? this.slideDown() :
+      this.startTouchEvent < this.endTouchEvent ? this.slideUp() :
+      false;
+    });
 
     this.root.addEventListener('wheel', e => {
-      if (e.deltaY > 0) {
-        // this.touchMove()
-        this.slideDown();
-      } else {
-        // this.touchMove()
-        this.slideUp();
-      }
+      e.deltaY > 0 ? this.slideDown() : this.slideUp();
     });
   }
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,18 @@
 import FullPageScroll from './FullPageScroll';
+import { TimelineMax } from 'gsap';
 
 const p = new FullPageScroll('#pagescroll', {
   dots: true,  // default false 
-  duration: 2000, // default 500
-  timingFunction: 'ease' //default linear
+  duration: 1000, // default 500
+  timingFunction: 'ease', //default linear
+  onLeave: () => {
+    const tl = new TimelineMax();
+
+    tl.fromTo(document.querySelectorAll('.section__title'), { y: -100, opacity: 0 }, { y: 0, opacity: 1 }, 1);
+  },
+  afterLoad: () => {
+    const tl = new TimelineMax();
+
+    tl.fromTo(document.querySelectorAll('.section__title'), { y: -100, opacity: 0 }, { y: 0, opacity: 1 });
+  }
 });
